13f5f87b955000bcb341e14dc99bd13d19f36bfa
hardik2598 <hardikbhatt2598@gmail.com>
2020-04-02 01:16:44 +0530

    Spring Security Configurations

--- src/main/java/com/postman/calenderslotbookingservice/controller/SampleController.java
+++ src/main/java/com/postman/calenderslotbookingservice/controller/SampleController.java
@@ -3,14 +3,15 @@ package com.postman.calenderslotbookingservice.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
 
 @Controller
 public class SampleController {
 
     @GetMapping({"/", "hello"})
-    public String helloWorld(@RequestParam(required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String helloWorld(Model model, Principal principal) {
+        model.addAttribute("name", principal.getName());
         return "hello-world";
     }
 }
