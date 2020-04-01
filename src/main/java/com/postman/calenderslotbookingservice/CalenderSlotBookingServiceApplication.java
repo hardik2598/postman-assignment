d841432feb54bc8e6cb7c3e6351b811796929b89
hardik2598 <hardikbhatt2598@gmail.com>
2020-04-02 00:51:12 +0530

    Initial Commit

---
+++ src/main/java/com/postman/calenderslotbookingservice/CalenderSlotBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.postman.calenderslotbookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenderSlotBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalenderSlotBookingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/postman/calenderslotbookingservice/CalenderSlotBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postman.calenderslotbookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalenderSlotBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
